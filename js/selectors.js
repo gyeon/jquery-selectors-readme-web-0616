2c57a772be402d187ebe909f506bfe4f358bf865
gyeon <gyeon19@gmail.com>
2016-07-22 00:38:03 -0400

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,24 @@
 // declare your functions here...
+function paragraphSelector() {
+  //use element selector for p tags and return
+  return $('p');
+}
+
+function lastImageSelector() {
+  return $('img:last');
+}
+
+function ninjaBabySelector() {
+  //use id selectors
+  return $('#baby-ninja');
+}
+
+function divSelector() {
+  //use class selector and return 2 divs on page
+  return $('div');
+}
+
+function firstListItem() {
+  //use first child selector return first item in ul with id pic-list
+  return $('ul#pic-list li:first-child');
+}
